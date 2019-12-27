5d725ad5f00a3f5fe9da6fd9ba7ca43285029c0f
SivaKarthikPara <sivakarthikpara@gmail.com>
2019-12-27 16:26:51 +0530

    Normal project

---
+++ Normal Project/src/com/LoggerMain.java
@@ -0,0 +1,24 @@
+package com;
+
+import java.util.logging.*;
+public class LoggerMain { 
+	public static void main(String[] args) 
+	{ 
+		DemoLogger obj = new DemoLogger(); 
+		obj.makeSomeLog(); 
+
+		// Generating some log messages through the 
+		// function defined above 
+		LogManager lgmngr = LogManager.getLogManager(); 
+
+		// lgmngr now contains a reference to the log manager. 
+		Logger log = lgmngr.getLogger(Logger.GLOBAL_LOGGER_NAME); 
+
+		// Getting the global application level logger 
+		// from the Java Log Manager 
+		log.log(Level.INFO, "This is a log message"); 
+
+		// Create a log message to be displayed 
+		// The message has a level of Info 
+	} 
+} 
---
+++ Normal Project/src/com/subArray.java
@@ -0,0 +1,11 @@
+package com;
+
+import java.util.Scanner;
+
+public class subArray {
+	public static void main(String [] args) {
+		Scanner input = new Scanner(System.in);
+		System.out.println("subArray");
+		input.close();
+	}
+}
